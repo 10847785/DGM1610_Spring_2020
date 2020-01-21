a191c3bc28221e221f59718011903f91d1730cb0
Eric Casper <eric322000@gmail.com>
2020-01-21 13:57:30 -0800

    Variables
    Put in some variable code

--- Unbelievable/Assets/Scripts/Assignments/Variables/Variables.cs
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    // Declaration
+    public int amount;
+    public float money;
+    public float cost;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cookies(amount, money, cost);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Cookies(int amount, float money, float cost);
+    {
+    float total;
+
+    total = money - cost* amount;
+
+    print("Hello World");
     }
 }

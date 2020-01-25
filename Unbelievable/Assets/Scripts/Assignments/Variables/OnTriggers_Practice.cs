7fbca7f9658a49f420ef4765bfc8862826cdd86b
Eric Casper <eric322000@gmail.com>
2020-01-24 19:52:46 -0800

    OnTrigger
    Practicing

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/OnTriggers_Practice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggers_Practice : MonoBehaviour
+{
+
+    // Declaration 
+    public int members;
+    public float mother;
+    public float father;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter(Collision other){
+
+        }
+    }
+}

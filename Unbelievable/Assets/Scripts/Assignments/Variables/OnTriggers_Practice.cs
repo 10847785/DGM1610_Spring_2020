bf12f4f39e85f5835712dba78bc4c21ac4d6eabe
Eric Casper <eric322000@gmail.com>
2020-01-24 20:25:01 -0800

    Stay and Exit
    Added those elements onto the triggers

--- Unbelievable/Assets/Scripts/Assignments/Variables/OnTriggers_Practice.cs
+++ Unbelievable/Assets/Scripts/Assignments/Variables/OnTriggers_Practice.cs
@@ -7,19 +7,27 @@ public class OnTriggers_Practice : MonoBehaviour
 
     // Declaration 
     public int members;
-    public float mother;
-    public float father;
+    public float alive;
+    public float dead;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(members 10, alive 10, dead 0); 
     }
 
     // Update is called once per frame
     void Update()
     {
         void OnTriggerEnter(Collision other){
+
+        }
+
+        void OnTriggerStay(Collision other){
+
+        }
+
+        void OnTriggerEnd(Collision other){
 
         }
     }

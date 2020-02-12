ff700016542093b6884936fc0ff6bdf73e01b909
Eric Casper <eric322000@gmail.com>
2020-02-12 16:57:49 -0700

    New Scripts
    Created health pickup scripts and spawnmanager and enemy move scripts

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/HealthPickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_PickUp : Pickup
+{
+    public int healthAmount = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Player health is" + healthAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug Gained" + healthAmount + " Health!");
+    }
+}
+
+public class Pickup
+{
+}

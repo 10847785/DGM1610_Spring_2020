fda318c0a8349fa7ec86a04fd3f258d1d22fde39
Eric Casper <eric322000@gmail.com>
2020-02-13 11:08:01 -0800

    Else Cause
    Practiced with else causes and fixed the health pickup bug.

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Else_Cause_Practice.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Else_Cause_Practice : MonoBehaviour
+{
+    public string TV;
+    public int amountTVs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This is not meant to be taken seriously, this is just for practice
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectables")) ;
+        {
+            else(amountTVs = 1);
+        }
+
+        else
+        {
+            if (amountTVs = 2) ;
+
+        }
+
+        if (other.gameObject.CompareTag("Collectable")) ;
+        {
+            amountTVs = 1;
+        }
+
+    
+        else 
+        {
+            amountTVs = 0;
+        }
+    }
+}
---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Health_PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_PickUp : PickUp
+{
+    public int healthAmount = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Player health is" + healthAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("You Gained" + healthAmount + "Health!");
+        Destroy(gameObject);
+    }
+}

34cda5c8ea67acb73031a735fef9c29abb492ca1
Eric Casper <eric322000@gmail.com>
2020-02-17 21:00:11 -0800

    Practice Scriptable objects
    Practicing this kind of code

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Script_Object_Practice.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Object_Practice : MonoBehaviour
+{
+    public int HealthToAdd = 5;
+
+    public static int Health;
+
+    public TextAlignment HealthText;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthText = (GetComponent<TextAlignment>());
+
+        Health = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health > 5) ;
+            Health = 5;
+    }
+}

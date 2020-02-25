d5afbccec584ada389aa6fe00bb680d40569c06a
Eric Casper <eric322000@gmail.com>
2020-02-24 17:12:26 -0700

    Worked with EnemyMove
    Updated enemyMove script

--- Unbelievable/Assets/Scripts/Assignments/Variables/EnemyMove.cs
+++ Unbelievable/Assets/Scripts/Assignments/Variables/EnemyMove.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public GameObject Player;
-    public Transform target;
-    public int damage;
+    //public Transform target; // transform.Translate example
+    //public int damage;
+    private Rigidbody enemyRb; // Velocity and Add Force example
     public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
         Player = GameObject.Find("Player");
-        
+        //target = GameObject.Find("Player").transform; // Transform.Translate example
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        // Transform.Translate
+        //transform.LookAt(target);
+        //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        // Add Force
+        enemyRb.AddForce((Player.transform.position - transform.position).normalized * moveSpeed);
+        // MovePosition
+
     }
 }
---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/PickUpAmmo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAmmo : MonoBehaviour
+{
+    public int AmmoPickUp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+ 
+        if (other.gameObject.CompareTag("PreFab"))
+        {
+           // AmmoManager.Ammo(AmmoPickUp);
+            Destroy(gameObject);
+        }
+    }
+
+        
+ 
+}

0cf63b764d0b7de268096eb070a1a3ce9dd595c5
Ericio <eric322000@gmail.com>
2020-03-03 13:34:02 -0700

    Practiced with  health management
    Created a healthmanager script

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/HealthManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static int playerHealth;
+    
+    
+
+    void Awake()
+    {
+        Time.timeScale = 1;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth > 5)
+            playerHealth = 5;
+
+        
+    }
+
+    public static void HealthAdd (int AddHealth)
+    {
+        playerHealth += AddHealth;
+    }
+
+    public void HealthSub (int SubHealth)
+    {
+        playerHealth -= SubHealth;
+    }
+}

b302627e519c2d51995687a6ba3431c168282503
Ericio <eric322000@gmail.com>
2020-03-03 17:11:12 -0700

    EnemyPatrolPractice
    Practiced with enemy patrol

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/EnemyPatrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public bool MoveAxis;
+
+
+    public Transform BoundryPoint;
+    public bool IsBoundry;
+
+
+    public Transform EdgeCheck;
+    private bool IsEdge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // IsEdge = Physics.(EdgeCheck.position, IsBoundry, BoundryPoint);
+
+       // IsBoundry = Physics.(BoundryPoint.position, IsBoundry, BoundryPoint);
+    }
+}

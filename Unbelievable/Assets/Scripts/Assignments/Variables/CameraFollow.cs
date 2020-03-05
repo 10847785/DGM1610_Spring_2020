7e8246a70d74d64bbbdee682f5de5be30beca801
Ericio <eric322000@gmail.com>
2020-03-04 20:53:06 -0700

    CameraFollow
    Added CameraFollow script.

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public CharacterController Player;
+
+    public float CameraSpeed = 1f;
+
+    // Camera positions
+
+    public float offSetX;
+    public float offSetY;
+    public float offSetZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float interpolation = CameraSpeed * Time.deltaTime;
+
+       transform.position = new Vector3(Player.transform.position.x + offSetX, Player.transform.position.y + offSetY, Player.transform.position.z + offSetZ);
+
+    } 
+}

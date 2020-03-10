dfe936edb85072ce45d2618112a9e6938fd66234
Ericio <eric322000@gmail.com>
2020-03-09 23:18:34 -0600

    Code for Loops
    Added code to the loops script

--- Unbelievable/Assets/Scripts/Assignments/Variables/Loops.cs
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Loops.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
+    public int Num = 5;
+
+    public string Enemies;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i <= 5; i++)
+        {
+            Debug.Log(i + "Counting Up");
+        }
+
+        while (Num > 0)
+        {
+          //  i < Enemies; i++[Debug.Log]
+            Debug.Log("Counting Down" + Num);
+            Num --;
+        }
+
+
     }
+
+  
 
     // Update is called once per frame
     void Update()

51f476b00976c30481add88d65f83f6f3b9e9c46
Ericio <eric322000@gmail.com>
2020-03-15 14:39:15 -0600

    Particle System
    Added particle system and destroy script.

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/ParticleDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroy : MonoBehaviour
+{
+    private ParticleSystem DestroyParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        DestroyParticles = GetComponent<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DestroyParticles.isEmitting)
+            return;
+
+       // GameObject Kill = (GameObject)Instantiate(transform.position, transform.rotation);
+
+        Destroy(gameObject);
+
+    }
+}

7c41aac6caf7338a3a38d0dd77b16a07e838f8b9
Ericio <eric322000@gmail.com>
2020-03-19 22:59:24 -0600

    Switch Statements
    Created switch statement script

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/SwitchStatement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchStatement : MonoBehaviour
+{
+    public int weaponType = 3;
+
+    void weaponsMenu()
+    {
+        switch (weaponType)
+        {
+            case 3:
+                print("Rocks");
+                break;
+
+            case 2:
+                print("Bow");
+                break;
+
+            case 1:
+                print("Sword");
+                break;
+
+            default:
+                print("Fists");
+                break;
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

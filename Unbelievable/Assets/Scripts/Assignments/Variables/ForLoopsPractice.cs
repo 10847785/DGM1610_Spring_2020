59c31e231a81d3140d604a716f03773c600dc12c
Ericio <eric322000@gmail.com>
2020-03-21 22:18:45 -0600

    ForLoops Practice
    Practiced with for loops

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/ForLoopsPractice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopsPractice : MonoBehaviour
+{
+    public int numStanleys;
+    public int numViolets;
+    public int numCarries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i <= numStanleys; i++)
+        {
+            Debug.Log("Stanleys are coming" + i);
+        }
+
+        for (int i = 0; i <= numViolets; i++)
+        {
+            Debug.Log("Violets are coming" + i);
+        }
+
+        for (int i = 0; i <= numCarries; i++)
+        {
+            Debug.Log("Carries are coming" + i);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

b0ba3c0145e0db5431c8c3ef10f4c039af0e6919
Ericio <eric322000@gmail.com>
2020-03-26 20:15:55 -0600

    Coroutine
    Made coroutine

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Coroutine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    public float move = 4f;
+    public Transform locate
+    {
+        get { return locate; }
+        set
+        {
+            locate = value;
+
+            StopCoroutine("Movement");
+           // StopCoroutine("Movement", locate);
+           // StopCoroutine locate isn't working
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MyCoroutine(locate));
+    }
+
+    IEnumerator MyCoroutine(Transform locate)
+    {
+
+        while (Vector3.Distance(transform.position, locate.position) > 1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, locate.position, move * Time.deltaTime);
+
+            yield return null;
+
+        }
+
+        print("Reached it's destined location");
+
+        //WaitForSeconds is not working 
+      //  yield return null WaitForSeconds(2f);
+
+
+        print("Finished");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

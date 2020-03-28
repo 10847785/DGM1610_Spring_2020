092618ead0f03292f0f215ae3b61abee3fe09cce
Ericio <eric322000@gmail.com>
2020-03-27 23:32:26 -0600

    Coroutine Practice
    Practiced with coroutines

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Coroutine1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine1 : MonoBehaviour
+{
+   /* public float Routine = 10f;
+    public Transform cycle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(MyCoroutine(cycle));
+
+    }
+
+    IEnumerator MyCoroutine(Transform cycle)
+
+    {
+
+        while (Vector3.Distance(transform.position, cycle.position) > 5f)
+        {
+
+            transform.position = Vector3.Lerp(transform.position, cycle.position, move * Time.deltaTime);
+
+            yield new null;
+        }
+
+        print("Reached it's detination");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}

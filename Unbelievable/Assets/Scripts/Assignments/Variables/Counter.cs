6b024325fb603942c21531bf989333011744a259
Ericio <eric322000@gmail.com>
2020-04-02 18:06:26 -0600

    Counter
    Created a counter script

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Counter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Counter : MonoBehaviour
+{
+    // During the game
+  //  public int countDownValue = 360;
+
+  //  public Text timer;
+
+    // Starts the game 
+    public float countDown;
+
+    public Text countText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // During the game
+        
+        // Starting the game
+        countDown -= Time.deltaTime;
+
+        countText.text = (countDown).ToString("0");
+
+        if (countDown < 0) ;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

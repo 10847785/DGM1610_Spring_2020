296c0295df9d72e48cb116e1b25aacf9a4844377
Ericio <eric322000@gmail.com>
2020-04-03 23:15:37 -0600

    TTimer Script
    Added a timer script

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // This timer counts down in game
+    public int countDownValue = 360;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

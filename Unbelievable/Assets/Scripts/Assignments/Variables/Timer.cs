af34622b02a20ee6479b745786325a7e3fc751d4
Ericio <eric322000@gmail.com>
2020-04-06 17:11:48 -0600

    NavMesh
    Created NavMesh and Wander script

--- Unbelievable/Assets/Scripts/Assignments/Variables/Timer.cs
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Timer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     // This timer counts down in game
+
     public int countDownValue = 360;
 
     // Start is called before the first frame update

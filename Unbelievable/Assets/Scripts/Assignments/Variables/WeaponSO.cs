ed8707583077d35241f0de8cfd148409cb5585a2
Ericio <eric322000@gmail.com>
2020-04-11 21:11:01 -0600

    Weapon SO
    Created weapons so

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/WeaponSO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SO", menuName = "Weapon Scriptable Object/New Weapon")]
+
+public class WeaponSO : ScriptableObject
+    
+{
+
+ //Weapons management
+    public string weapon;
+
+    public string description;
+
+    public GameObject model;
+
+    public Projectiles type;
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

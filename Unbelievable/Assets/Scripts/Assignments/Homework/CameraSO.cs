bf493bbddf74153a4a27c328000a1cdc504a1a93
Ericio <eric322000@gmail.com>
2020-04-12 22:27:03 -0600

    Camera SO
    Made a camera SO

---
+++ Unbelievable/Assets/Scripts/Assignments/Homework/CameraSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SO", menuName = "Camera Scriptable Object/New Camera")]
+
+
+public class CameraSO : ScriptableObject
+{
+
+    public new string camera;
+
+    public Vector3 Position;
+
+    public Vector3 Scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unbelievable/Assets/Scripts/Assignments/Homework/Camera_Object.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Object : MonoBehaviour
+{
+
+  //  public CameraSO data;
+
+  //  private GameObject model;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+   //     model = data.model;
+
+   //     var modelRend = model.GetComponent<Renderer>();
+   //     modelRend.sharedMaterial
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

2a996ac8c734de53d2e38ceb3b69a5cdfb768424
Ericio <eric322000@gmail.com>
2020-04-15 17:25:13 -0600

    Player Health
    Add components for player health

---
+++ Unbelievable/Assets/Scripts/Assignments/Variables/Player_Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+
+    public int currentHealth = maxHealth;
+
+    public Text hp;
+
+    public Text maxHP;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp.text = currentHealth.ToString();
+        maxHP.text = maxHealth.ToString();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            print("You are Dead! Dead! Dead!");
+        }
+    }
+}

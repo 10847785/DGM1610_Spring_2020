2035e0bf3359bc6474fcebf5a4bb6212536c91d1
Ericio <eric322000@gmail.com>
2020-04-15 23:04:55 -0600

    Created power up animation
    Made a power up animation

---
+++ Unbelievable/Assets/Scripts/Assignments/Homework/PowerUpAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpAnimation : MonoBehaviour
+{
+    public float z_speed = 3f;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, z_speed);
+    }
+}
